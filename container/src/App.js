50e4d08f89c3f118bc2ea720ed93436e9ad5ac2a
anhtudo97 <anhtudo97@gmail.com>
2021-07-31 15:00:15 +0700

    feat: setup container

---
+++ container/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
+
+export default () => {
+  return (
+    <div>
+      <h1>Container</h1>
+    </div>
+  );
+};
