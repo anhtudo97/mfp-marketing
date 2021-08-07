817034acc2286f5041a245d22f896d973f071f17
anhtudo97 <anhtudo97@gmail.com>
2021-08-08 01:26:15 +0700

    chore: update

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// Get root
 const root = document.querySelector("#root");
 
-ReactDOM.render(<App />, root);
+ReactDOM.render( < App / > , root);
