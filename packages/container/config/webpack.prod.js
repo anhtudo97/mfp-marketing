1faec915feb9d7dc62290c4cda583e0506993847
anhtudo97 <anhtudo97@gmail.com>
2021-08-08 16:40:03 +0700

    feat: remove

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,7 +3,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-// Domain setting
+// Domain 
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
