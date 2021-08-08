b02471b76e86d65bde9a4a7add130d3bfd41ef15
anhtudo97 <anhtudo97@gmail.com>
2021-08-08 16:36:31 +0700

    feat: add setting

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,6 +3,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
+// Domain setting
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
